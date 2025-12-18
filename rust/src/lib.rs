381ae9120f2f414a64999ea477e3b35ce6325ee4
Jacob-Walton <jacob-walton@konpeki.co.uk>
2025-12-18 17:42:22 +0000

    chore: update lib definitions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,12 +6,16 @@ use pyo3::prelude::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
-mod wry_py {
-    use pyo3::prelude::*;
+fn wry_py(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    // Classes
+    m.add_class::<elements::Element>()?;
+    m.add_class::<elements::ElementBuilder>()?;
 
-    /// Formats the sum of two numbers as string.
-    #[pyfunction]
-    fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-        Ok((a + b).to_string())
-    }
+    // Convenience functions
+    m.add_function(wrap_pyfunction!(elements::div, m)?)?;
+    m.add_function(wrap_pyfunction!(elements::text, m)?)?;
+    m.add_function(wrap_pyfunction!(elements::button, m)?)?;
+    m.add_function(wrap_pyfunction!(elements::input, m)?)?;
+
+    Ok(())
 }
